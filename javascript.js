6364842e6f0424ad5533bca2b3a51a347cfcffb5
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-10-17 16:47:00 +0300

    created a radio input method for the gender variable

--- javascript.js
+++ javascript.js
@@ -1,11 +1,15 @@
 function compute()
 	{let years = document.getElementById("year").value;
-     CC = years.string(0,1)
+     cc = years.slice(0,2)
+     yy = years.slice(2,4)
 	var months = document.getElementById("month").value;
-
+     mm = years.slice(0,2)
 	var dates = document.getElementById("date").value;
-
+     dd = years.slice(0,2)
     var genders = document.getElementById("gender").value;
 
-	
-	}
+    console.log(cc);
+    }
+
+    compute()
+
