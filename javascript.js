8f6285f3b0949d719d203563f6593767be3c6489
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-10-17 03:07:03 +0300

    i have added a code for the user to be able to input their data

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function compute()
+	{let years = document.getElementById("year").value;
+     CC = years.string(0,1)
+	var months = document.getElementById("month").value;
+
+	var dates = document.getElementById("date").value;
+
+    var genders = document.getElementById("gender").value;
+
+	
+	}
